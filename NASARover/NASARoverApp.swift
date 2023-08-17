2ae8d50daac78e6791ea5eafdebebdf5bd240e2e
d.pechenenko <pechenenkod@gmail.com>
2023-08-17 18:01:35 +0200

    Initial Commit

---
+++ NASARover/NASARoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASARoverApp.swift
+//  NASARover
+//
+//  Created by Dmitro Pechenenko on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NASARoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
